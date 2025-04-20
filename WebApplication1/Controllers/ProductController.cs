98225e12109a70669077ce5883de5bc72ca5d576
Taha Yasin Çiçek <116304917+tahayasincicek@users.noreply.github.com>
2025-04-20 15:58:38 +0300

    Add files via upload

---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UrunAPI.Data;
+using UrunAPI.Models;
+
+namespace UrunAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly DbHelper _db;
+
+        public ProductController(DbHelper db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_db.GetAllProducts());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var product = _db.GetProductById(id);
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            _db.AddProduct(product);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Product product)
+        {
+            _db.UpdateProduct(id, product);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _db.DeleteProduct(id);
+            return Ok();
+        }
+    }
+}
---
+++ WebApplication1/Data/DbHelper.cs
@@ -0,0 +1,101 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using UrunAPI.Models;
+
+namespace UrunAPI.Data
+{
+    public class DbHelper
+    {
+        private readonly string _connectionString;
+
+        public DbHelper(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            var products = new List<Product>();
+
+            using SqlConnection conn = new SqlConnection(_connectionString);
+            using SqlCommand cmd = new SqlCommand("SELECT * FROM Products", conn);
+
+            conn.Open();
+            using SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                products.Add(new Product
+                {
+                    Id = (int)reader["Id"],
+                    Name = reader["Name"].ToString()!,
+                    Price = (decimal)reader["Price"],
+                    Stock = (int)reader["Stock"]
+                });
+            }
+
+            return products;
+        }
+
+        public Product? GetProductById(int id)
+        {
+            using SqlConnection conn = new SqlConnection(_connectionString);
+            using SqlCommand cmd = new SqlCommand("SELECT * FROM Products WHERE Id = @Id", conn);
+
+            cmd.Parameters.AddWithValue("@Id", id);
+            conn.Open();
+
+            using SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.Read())
+            {
+                return new Product
+                {
+                    Id = (int)reader["Id"],
+                    Name = reader["Name"].ToString()!,
+                    Price = (decimal)reader["Price"],
+                    Stock = (int)reader["Stock"]
+                };
+            }
+
+            return null;
+        }
+
+        public void AddProduct(Product product)
+        {
+            using SqlConnection conn = new SqlConnection(_connectionString);
+            using SqlCommand cmd = new SqlCommand("INSERT INTO Products (Name, Price, Stock) VALUES (@Name, @Price, @Stock)", conn);
+
+            cmd.Parameters.AddWithValue("@Name", product.Name);
+            cmd.Parameters.AddWithValue("@Price", product.Price);
+            cmd.Parameters.AddWithValue("@Stock", product.Stock);
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+        }
+
+        public void UpdateProduct(int id, Product product)
+        {
+            using SqlConnection conn = new SqlConnection(_connectionString);
+            using SqlCommand cmd = new SqlCommand("UPDATE Products SET Name = @Name, Price = @Price, Stock = @Stock WHERE Id = @Id", conn);
+
+            cmd.Parameters.AddWithValue("@Id", id);
+            cmd.Parameters.AddWithValue("@Name", product.Name);
+            cmd.Parameters.AddWithValue("@Price", product.Price);
+            cmd.Parameters.AddWithValue("@Stock", product.Stock);
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+        }
+
+        public void DeleteProduct(int id)
+        {
+            using SqlConnection conn = new SqlConnection(_connectionString);
+            using SqlCommand cmd = new SqlCommand("DELETE FROM Products WHERE Id = @Id", conn);
+
+            cmd.Parameters.AddWithValue("@Id", id);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
